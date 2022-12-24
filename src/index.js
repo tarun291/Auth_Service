14b323118a361be2e9249c14dbd641bba20c36a4
tarun291 <tarunkumar222020@gmail.com>
2022-12-24 14:23:53 +0530

    Added jwt create and verify logic

--- src/index.js
+++ src/index.js
@@ -6,13 +6,8 @@ const { PORT } = require('./config/serverConfig');
 
 const apiRoutes = require('./routes/index');
 
-// const { User } = require('./models/index');
-
-// const bcrypt = require('bcrypt');
-
-// const UserRepository=require('./repository/user-repository');
-
-const app=express();
+const UserService = require('./services/user-service');
+const app = express();
 
 const prepareAndStartServer = () => {
     app.use(bodyParser.json());
@@ -21,12 +16,11 @@ const prepareAndStartServer = () => {
 
     app.listen(PORT, async () => {
         console.log(`Server Started on Port:${PORT}`);
-        // const repo=new UserRepository();
-        // const response=await repo.getById(9);
-        // console.log(response);
-        // const incommingPassword = "Sarthak";
-        // const user = await User.findByPk(9);
-        // const response = bcrypt.compareSync(incommingPassword, user.password);
+        const service = new UserService();
+        // const newToken=service.createToken({email:"Tarun@gmail.com",id:1});
+        // console.log(newToken);
+        // const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6IlRhcnVuQGdtYWlsLmNvbSIsImlkIjoxLCJpYXQiOjE2NzE4NzE2OTUsImV4cCI6MTY3MTg3MTcyNX0.wuR-WlRYGgUVYMBZcVD-iqYPzPexN5tnONXN9C01-3o";
+        // const response = service.veryfyToken(token);
         // console.log(response);
     })
 }
--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,17 +1,40 @@
-const UserRepository=require('../repository/user-repository');
+const jwt = require('jsonwebtoken');
 
-class UserService{
-    constructor(){
-        this.userRepository=new UserRepository();
+
+const UserRepository = require('../repository/user-repository');
+
+const { JWT_KEY } = require('../config/serverConfig');
+
+class UserService {
+    constructor() {
+        this.userRepository = new UserRepository();
     }
-    async create(data){
+    async create(data) {
         try {
-            const user=await this.userRepository.create(data);
+            const user = await this.userRepository.create(data);
             return user;
         } catch (error) {
             console.log("Something went wrong in service layer");
             throw error;
         }
     }
+    createToken(user) {
+        try {
+            const result = jwt.sign(user,JWT_KEY,{expiresIn:"1h"});
+            return result;
+        } catch (error) {
+            console.log("Something went wrong in token creation");
+            throw error;
+        }
+    }
+    veryfyToken(token){
+        try {
+            const response=jwt.verify(token,JWT_KEY);
+            return response;
+        } catch (error) {
+            console.log("Something went wrong in token validation",error);
+            throw error;
+        }
+    }
 }
-module.exports=UserService;
+module.exports = UserService;
